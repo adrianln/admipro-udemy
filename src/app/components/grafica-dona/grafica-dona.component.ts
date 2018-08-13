c4155e47b681cd2bf0ed11f3980646ee1a8c680a
Adrian Leon <aleonhost@gmail.com>
2018-08-12 22:32:51 -0300

    Input, output y viewchild

---
+++ src/app/components/grafica-dona/grafica-dona.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-grafica-dona',
+  templateUrl: './grafica-dona.component.html',
+  styles: []
+})
+export class GraficaDonaComponent implements OnInit {
+
+ @Input('chartLabels') public doughnutChartLabels: string[] = [];
+ @Input('chartData') public doughnutChartData: number[] = [];
+ @Input('chartType') public doughnutChartType: string = '';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,3 +1,4 @@
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { ProgressComponent } from './progress/progress.component';
@@ -5,13 +6,23 @@ import { Graficas1Component } from './graficas1/graficas1.component';
 import { PagesComponent } from './pages.component';
 import { SharedModule } from '../shared/shared.module';
 import { PAGES_ROUTES } from './pages.routes';
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
+
+// ng2-charts
+
+import { ChartsModule } from 'ng2-charts';
+
+import { GraficaDonaComponent } from '../components/grafica-dona/grafica-dona.component';
+
 
 @NgModule({
     declarations: [
         PagesComponent,
         DashboardComponent,
         ProgressComponent,
-        Graficas1Component
+        Graficas1Component,
+        IncrementadorComponent,
+        GraficaDonaComponent
     ],
     exports: [
         DashboardComponent,
@@ -20,7 +31,9 @@ import { PAGES_ROUTES } from './pages.routes';
     ],
     imports: [
         SharedModule,
-        PAGES_ROUTES
+        PAGES_ROUTES,
+        FormsModule,
+        ChartsModule
     ]
 })
 export class PagesModule { }
